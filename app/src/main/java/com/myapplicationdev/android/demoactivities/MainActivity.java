80bfedb56d1c20f77f63d764a6cc0d8914fc09c6
18016624 <18016624@myrp.edu.sg>
2020-04-24 12:54:34 +0800

    done

--- app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
@@ -2,7 +2,12 @@ package com.myapplicationdev.android.demoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnDone = (Button) findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etName = (EditText) findViewById(R.id.editTextName);
+                EditText etAge = (EditText) findViewById(R.id.editTextAge);
+                String[] info = {etName.getText().toString(),
+                etAge.getText().toString()};
+
+                Intent i = new Intent(DemoActivities.this,
+                        DemoActivities2.class);
+
+                i.putExtra("info",info);
+                startActivity(i);
+            }
+        });
     }
 }
